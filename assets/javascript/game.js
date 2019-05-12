9765cfdea8a10453fd4d4459d5360657b90d3ee2
Juwami <sean.nguyenf@yahoo.com>
2019-05-12 00:47:21 -0400

    added extra rupee, created for function to randomize rupee values

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,26 +7,7 @@ let wins = 0
 let losses = 0
 let userScore = 0
 let winningNumber = changeWinningNumber
-
-$('.rupees').on('click', function() {
-    $('#blueRupee').on('click', addBlueValue)
-    $('#greenRupee').on('click', addGreenValue)
-    $('#orangeRupee').on('click', addOrangeValue)
-    $('#purpleRupee').on('click', addPurpleValue)
-//     $('redRupee').on('click', addRedValue)
-//     $('silverRupee').on('click', addSilverValue)
-//     $('yellowRupee').on('click', addYellowValue)
-
-if (userScore === winningNumber) {
-    wins++
-    $('winsText').html('Wins: ' + wins)
-    changeWinningNumber()
-} else if (userScore > winningNumber) {
-    losses++
-    $('lossesText').html('Losses: ' + losses)
-    changeWinningNumber()
-}
-})
+let gemValues = []
 
 $('#neededScore').html(winningNumber)
 
@@ -35,5 +16,52 @@ function changeWinningNumber() {
     return num
 }
 
+function resetGemValues() {
+    for (i=0; i<5; i++) {
+        gemValues.push(Math.floor(Math.random() * 12)+ 1)
+        }
+}
+
+for (i=0; i<5; i++) {
+gemValues.push(Math.floor(Math.random() * 12)+ 1)
+}
+// Below line is for testing gemValues
+console.log(gemValues)
+
+$('#blueRupee').on('click', addBlueValue)
+$('#greenRupee').on('click', addGreenValue)
+$('#orangeRupee').on('click', addOrangeValue)
+$('#purpleRupee').on('click', addPurpleValue)
+$('#redRupee').on('click', addRedValue)
 
 
+// if (userScore === winningNumber) {
+//     wins++
+//     $('winsText').html('Wins: ' + wins)
+//     changeWinningNumber()
+// } else if (userScore > winningNumber) {
+//     losses++
+//     $('lossesText').html('Losses: ' + losses)
+//     changeWinningNumber()
+// }
+
+function addBlueValue() {
+    userScore = userScore + gemValues[0]
+    $('#totalScore').html(userScore)
+}
+function addGreenValue() {
+    userScore = userScore + gemValues[1]
+    $('#totalScore').html(userScore)
+}
+function addOrangeValue() {
+    userScore = userScore + gemValues[2]
+    $('#totalScore').html(userScore)
+}
+function addPurpleValue() {
+    userScore = userScore + gemValues[3]
+    $('#totalScore').html(userScore)
+}
+function addRedValue() {
+    userScore = userScore + gemValues[4]
+    $('#totalScore').html(userScore)
+}
